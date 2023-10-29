b0515c2a774bb50bfdf00590295c929d869de078
Aleksei-Kutuzov <Alekseikutuzov2@yandex.com>
2023-10-29 15:00:14 +0300

    it is all libraries

---
+++ libraries.py
@@ -0,0 +1,13 @@
+import json
+import sys
+import sqlite3
+from sqlite3 import Error
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QHBoxLayout, QLabel
+from PyQt5 import QtGui, QtBluetooth
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import traceback
+import datetime
+from PIL import Image
